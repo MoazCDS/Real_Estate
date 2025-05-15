1f277470faabccf5b3be8acddcf5c5c928c10f7e
Moaz Elbahr <moazelbahr@gmail.com>
2025-05-15 11:49:10 +0300

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name' : "App One",
+    'author' : "Moaz Elbahr",
+    'category': '',
+    'version': '17.0.0.1.0',
+    'depends': ['base',
+                ],
+    'data': [
+        'views/base_menu.xml',
+        'views/property_view.xml',
+        'views/owner_view.xml',
+        'views/tag_view.xml',
+        'security/ir.model.access.csv'
+    ],
+    'application': True,
+}
---
+++ models/property.py
@@ -0,0 +1,40 @@
+from email.policy import default
+
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+
+class Property(models.Model):
+    _name = 'property'
+
+    name = fields.Char(default="New", size=10)
+    description = fields.Text()
+    postcode = fields.Char(required=True)
+    date_availability = fields.Date()
+    expected_price = fields.Float(digits=(0,5 ))
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection([
+        ('north', 'North'),
+        ('south', 'South'),
+        ('east', 'East'),
+        ('west', 'West')
+    ], default="north")
+
+    owner_id = fields.Many2one('owner')
+    tag_ids = fields.Many2many('tag')
+
+    _sql_constraints = [
+        ('name_key', 'unique("name")', 'This name already exists')
+    ]
+
+    @api.constrains('bedrooms')
+    def _check_bedrooms_greater_zero(self):
+        for rec in self:
+            if rec.bedrooms <= 0:
+                raise ValidationError('Please enter a valid number of bedrooms')
